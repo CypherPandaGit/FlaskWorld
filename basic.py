828cef53e62ea5622eea80ede6768135e4d8ae46
Roman <cypherpanda@pm.me>
2020-04-01 15:13:12 +0200

    flask exercise

--- basic.py
+++ basic.py
@@ -2,14 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    my_list = [1, 2, 3, 4, 5]
-    puppy_list = ['Fluffy', 'Rufus', 'Spike']
-    user_logged_in = True
+    return render_template('home.html')
 
-    return render_template('basic.html', my_list = my_list, puppy_list = puppy_list,
-                           user_logged_in = user_logged_in)
+
+@app.route('/puppy/<name>')
+def pup_name(name):
+    return render_template('puppy.html', name=name)
 
 
 if __name__ == '__main__':
