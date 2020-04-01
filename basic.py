434871b3ecb3f5d5213a44ff8f5e44afbd5cb7ba
Roman <cypherpanda@pm.me>
2020-04-01 14:40:58 +0200

    flask exercise

--- basic.py
+++ basic.py
@@ -6,8 +6,10 @@ app = Flask(__name__)
 def index():
     my_list = [1, 2, 3, 4, 5]
     puppy_list = ['Fluffy', 'Rufus', 'Spike']
+    user_logged_in = True
 
-    return render_template('basic.html', my_list = my_list, puppy_list = puppy_list)
+    return render_template('basic.html', my_list = my_list, puppy_list = puppy_list,
+                           user_logged_in = user_logged_in)
 
 
 if __name__ == '__main__':
