ad5d25cca9544c1c980d7eff89421e5d18527841
Roman <cypherpanda@pm.me>
2020-04-02 18:26:18 +0200

    challenge

--- exercise/app.py
+++ exercise/app.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
 
     return render_template('index.html')
+
 
 @app.route('/report')
 def report():
@@ -22,9 +24,9 @@ def report():
 
     report = lower_letter and upper_letter and num_end
 
-    
     return render_template('report.html', report=report, lower=lower_letter,
                            upper=upper_letter, num_end=num_end)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
