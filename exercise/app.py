e106c7468637578a5f9a912067746620f9e4bd8d
Roman <cypherpanda@pm.me>
2020-04-02 18:10:19 +0200

    challenge

--- exercise/app.py
+++ exercise/app.py
@@ -9,8 +9,22 @@ def index():
 
 @app.route('/report')
 def report():
+
+    lower_letter = False
+    upper_letter = False
+    num_end = False
+
     user_name = request.args.get('user_name')
-    return render_template('report.html', user_name=user_name)
+    
+    lower_letter = any(c.islower() for c in user_name)
+    upper_letter = any(c.isupper() for c in user_name)
+    num_end = user_name[-1].isdigit()
+
+    report = lower_letter and upper_letter and num_end
+
+    
+    return render_template('report.html', report=report, lower=lower_letter,
+                           upper=upper_letter, num_end=num_end)
 
 if __name__ == '__main__':
     app.run(debug=True)
