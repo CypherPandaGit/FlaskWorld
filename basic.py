3205fbb76850d0123855ad2e2ecc6f3f0c662940
Roman <cypherpanda@pm.me>
2020-04-02 13:14:08 +0200

    flask exercise

--- basic.py
+++ basic.py
@@ -2,15 +2,20 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('index.html')
 
+@app.route('/signup_form')
+def signup_form():
+    return render_template('signup.html')
 
-@app.route('/puppy/<name>')
-def pup_name(name):
-    return render_template('puppy.html', name=name)
+@app.route('/thank_you')
+def thank_you():
+    first = request.args.get('first')
+    last = request.args.get('last')
+
+    return render_template('thankyou.html', first=first, last=last)
 
 
 if __name__ == '__main__':
