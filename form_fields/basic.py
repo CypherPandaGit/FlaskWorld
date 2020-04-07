f46f39ee4e0be247756b93f9bbca044fc4504b37
Roman <cypherpanda@pm.me>
2020-04-07 20:29:50 +0200

    form fields

--- form_fields/basic.py
+++ form_fields/basic.py
@@ -11,4 +11,13 @@ app.config['SECRET_KEY'] = 'mykey'
 
 class InfoForm(FlaskForm):
 
-    breed = StringField('What breed are you?', validators=[])
+    breed = StringField('What breed are you?', validators=[DataRequired()])
+    neutered = BooleanField("Have you been neutered?")
+    mood = RadioField('Please choose your mood: ',
+                      choices=[('mood_one', 'Happy'),('mood_two', 'Excited')])
+    food_choice = SelectField(u'Pick your favorite food: ',
+                              choices=[('chi','Chicken'),('bf','Beef'),
+                                       ('fish','Fish')])
+    feedback = TextAreaField()
+    submit = SubmitField('Submit')
+
