bfc9ec83cf79cf6c7f91312bdca1b891ddd1fa91
Roman <cypherpanda@pm.me>
2020-04-07 20:40:47 +0200

    form fields

--- form_fields/basic.py
+++ form_fields/basic.py
@@ -14,10 +14,34 @@ class InfoForm(FlaskForm):
     breed = StringField('What breed are you?', validators=[DataRequired()])
     neutered = BooleanField("Have you been neutered?")
     mood = RadioField('Please choose your mood: ',
-                      choices=[('mood_one', 'Happy'),('mood_two', 'Excited')])
+                      choices=[('mood_one', 'Happy'), ('mood_two', 'Excited')])
     food_choice = SelectField(u'Pick your favorite food: ',
-                              choices=[('chi','Chicken'),('bf','Beef'),
-                                       ('fish','Fish')])
+                              choices=[('chi', 'Chicken'), ('bf', 'Beef'),
+                                       ('fish', 'Fish')])
     feedback = TextAreaField()
     submit = SubmitField('Submit')
 
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    form = InfoForm()
+    if form.validate_on_submit():
+        session['breed'] = form.breed.data
+        session['neutered'] = form.neutered.data
+        session['mood'] = form.mood.data
+        session['food'] = form.food_choice.data
+        session['feedback'] = form.feedback.data
+
+        return redirect(url_for('thank_you'))
+
+    return render_template('index.html', form=form)
+
+
+@app.route('/thank_you')
+def thank_you():
+    return render_template('thank_you.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
