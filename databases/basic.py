bde93a2030d1f5f666285a1b37af65a96df1f65c
Roman <cypherpanda@pm.me>
2020-04-09 21:43:19 +0200

    SQL; setup database

--- databases/basic.py
+++ databases/basic.py
@@ -3,4 +3,32 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-# __file__ --> C://User/someuser/Projects/basic.py
+# __file__ --> C://User/SomeUser/Projects/basic.py // it works on every OS
+print(basedir)
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+######################################################################
+
+
+class Pokemon(db.Model):
+
+    # MANUAL TABLE NAME CHOICE
+    __tablename__ = 'pokemon'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    level = db.Column(db.Integer)
+
+    def __init__(self,name,level):
+        self.name = name
+        self.level = level
+
+    def __repr__(self):
+        return f"Pokemon {self.name} is level {self.level}"
+
