13d83a4ff312bccbf3cfad4d88c643eb8c34a8e5
Roman <cypherpanda@pm.me>
2020-04-10 14:16:34 +0200

    SQL; crud

---
+++ relationships/models.py
@@ -0,0 +1,9 @@
+# MODELS.PY
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
